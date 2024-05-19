6a5ab11e29db928cf2ae534116dc4d7fa829df4e
gv <151510250+Gokulvijayvp@users.noreply.github.com>
2024-05-19 19:23:48 +0530

    Update CourseEditForm.js

--- client/src/components/CourseEditForm.js
+++ client/src/components/CourseEditForm.js
@@ -1,67 +1,99 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { UseStudentContext } from '../mycontext/context';
 import { RiCloseFill } from "react-icons/ri";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const CourseEditForm = () => {
-  const { courseData, setCourse, course, setOpenUp, courseId, handleUpdateCourse } = UseStudentContext()
+  const {courseData, setOpenUp, courseId,courseSchema, handleUpdateCourse} = UseStudentContext()
+  const { register, handleSubmit, formState: { errors },} = useForm({resolver: yupResolver(courseSchema),});
   const id = courseId
   const usersData = courseData.find((course) => course._id.toString() === id)
 
-  useEffect(() => {
-    if (usersData) {
-      setCourse(prevUsers => ({
-        ...prevUsers,
-        name: usersData.name,
-        email: usersData.email,
-        course: usersData.course,
-        date: usersData.date,
-      }));
-    }
-  }, [usersData, setCourse]);
+  const onSubmit = async(data) => {
+    const updatedCourse = {
+      name: data.name,
+      email: data.email,
+      course: data.course,
+    };
+
+    await handleUpdateCourse(usersData._id, updatedCourse);
+  };
+  
   return (
     <div>
-      <div className='modelOverview'>
-        <div className=' model'>
-          <div className='col-10 float-start header borderBottom p-3'>
-            <h4 className='textblack float-start mb-0'>Update Course</h4>
-            <button className='float-end btn btn-outline-secondary btn-sm' onClick={() => setOpenUp(false)}>  <RiCloseFill className="my-1"/></button>
-          </div>
-          <form className='body col-10 float-start p-3'>
-            <div className="mb-3 col-12 d-flex align-items-center">
+      {usersData && 
+        <div className='modelOverview'>
+          <div className=' model'>
+            <div className='col-10 float-start header borderBottom p-3'>
+              <h4 className='textblack float-start mb-0'>Update Course</h4>
+              <button className='float-end btn btn-outline-secondary btn-sm' onClick={() => setOpenUp(false)}>  <RiCloseFill className="my-1"/></button>
+            </div>
+            <form className='body col-10 float-start p-3' onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-3 col-12 d-flex align-items-center">
                 <label htmlFor="name" className="form-label mb-0 col-4 fw-bold">
-                  Name
+                    Name
                 </label>
                 <div className="col-8">
-                  <input className='form-control mb-2' name='name' id='name' placeholder='name' value={course.name} onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+                    <input className='form-control mb-2' name='name' id='name' placeholder='name' {...register("name")} defaultValue={usersData.name || ''} />
+                    {errors?.name && (
+                      <div className="form-text error">
+                        {errors.name?.message}
+                      </div>
+                    )}
                 </div>
-            </div>
-
-            <div className="mb-3 col-12 d-flex align-items-center">
-                <label htmlFor="email" className="form-label mb-0 col-4 fw-bold">
-                  Email
-                </label>
-                <div className="col-8">
-                  <input className='form-control mb-2' name='email' id='email' placeholder='email' value={course.email} onChange={(e) => setCourse({ ...course, email: e.target.value })} />
+              </div>
+              <div className="mb-3 col-12 d-flex align-items-center">
+                  <label htmlFor="email" className="form-label col-4 mb-0 fw-bold">
+                    Email address
+                  </label>
+                  <div className="col-8">
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Please enter the email"
+                      id="email"
+                      name="email"
+                      defaultValue={usersData.email || '' } 
+                      {...register("email")}
+                    />
+                    {errors?.email && (
+                      <div className="form-text error">
+                        {errors.email?.message}
+                      </div>
+                    )}
+                  </div>
                 </div>
-            </div>
-            
-            <div className="mb-3 col-12 d-flex align-items-center">
-                <label htmlFor="course" className="form-label mb-0 col-4 fw-bold">
-                  Course
-                </label>
-                <div className="col-8">
-                  <input className='form-control mb-2' name='course' id='course' type='text' placeholder='course' value={course.course} onChange={(e) => setCourse({ ...course, course: e.target.value })} />
+                <div className="mb-3 col-12 d-flex align-items-center">
+                  <label htmlFor="course" className="form-label mb-0 col-4 fw-bold">
+                    Course
+                  </label>
+                  <div className="col-8">
+                  <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Please enter the course"
+                      id="course"
+                      name="course"
+                      defaultValue={usersData.course || ''} 
+                      {...register("course")}
+                    />
+                    {errors?.course && (
+                      <div className="form-text error">
+                        {errors.course?.message}
+                      </div>
+                    )}                  
+                  </div> 
                 </div>
-            </div>
 
-            <div className='footer col-12 float-start p-3 borderTop'>
-              <button className='btn btn-primary btn-sm float-end ms-2' onClick={() => handleUpdateCourse(usersData._id)}>Update</button>
-              <button className='btn btn-outline-secondary btn-sm float-end' onClick={() => setOpenUp(false)}>Close</button>
-            </div>
-          </form>
-
+              <div className='footer col-12 float-start p-3 borderTop'>
+                <button className='btn btn-primary btn-sm float-end ms-2' type="submit">Update</button>
+                <button className='btn btn-outline-secondary btn-sm float-end' onClick={() => setOpenUp(false)}>Close</button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      }
     </div>
   )
 }
